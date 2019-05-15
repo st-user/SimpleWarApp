d82fa1659a7a1d6375747da4cc7b75322494781d
st user <nck.cd.339@gmail.com>
2019-05-16 00:03:29 +0900

    refine test cases.

--- src/main/java/com/example/InitServletContextListener.java
+++ src/main/java/com/example/InitServletContextListener.java
@@ -9,5 +9,6 @@ public class InitServletContextListener implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		sce.getServletContext().setRequestCharacterEncoding("UTF-8");
+		sce.getServletContext().setResponseCharacterEncoding("UTF-8");
 	}
 }
--- src/main/java/com/example/SimpleServlet.java
+++ src/main/java/com/example/SimpleServlet.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,15 +15,30 @@ public class SimpleServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		 req.setCharacterEncoding("UTF-8");
 		System.out.println("requestCharacterEncoding : " + req.getServletContext().getRequestCharacterEncoding());
 		System.out.println("req.getCharacterEncoding() : " + req.getCharacterEncoding());
+		System.out.println("responseCharacterEncoding : " + req.getServletContext().getResponseCharacterEncoding());
+		System.out.println("resp.getCharacterEncoding() : " + resp.getCharacterEncoding());
 
 		String hello = req.getParameter("hello");
 		if (hello != null) {
-			System.out.println("hello : " + req.getParameter("hello"));
+			System.out.println("hello( expected:E3 81 82 ) : " + toHexString(req.getParameter("hello")));
 		} else {
-			System.out.println("body : " + req.getReader().readLine());
+			System.out.println("body( expected:E3 81 82 ) : " + toHexString(req.getReader().readLine()));
 		}
+		resp.getWriter().println("あ");
+	}
+
+	// To avoid the platform default encoding to affect assertion, I check the text in hexadecimal format.
+	private String toHexString(String str) throws UnsupportedEncodingException {
+		String ret = "";
+		for (int b : str.getBytes("UTF-8")) {
+			if (b < 256) {
+				b += 256;
+			}
+			ret += " ";
+			ret += Integer.toHexString(b).toUpperCase();
+		}
+		return ret;
 	}
 }
